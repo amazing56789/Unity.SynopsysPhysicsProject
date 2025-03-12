4d8f1f2dd58c836828ae01069f8eff2d964c1482
amazing56789 <pranavburra+git@gmail.com>
2025-03-11 23:36:21 -0700

    Going back to fix particle collisions

--- Assets/_Scripts/DeformableContactObjects.cs
+++ Assets/_Scripts/DeformableContactObjects.cs
@@ -1,3 +1,4 @@
+[System.Serializable]
 public struct DeformableContactObject {
     // #@TODO: add types to var names
     public float yieldStrength;
--- Assets/_Scripts/NonDeformableContactObject.cs
+++ Assets/_Scripts/NonDeformableContactObject.cs
@@ -1,3 +1,4 @@
+[System.Serializable]
 public struct NonDeformableContactObject {
     // #@TODO: add types to var names
     public float youngModulus;
